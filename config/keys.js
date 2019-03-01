e2a6aeda6fa26b0d9286a5921acd9f8f7790a577
rakane <rakane@syr.edu>
2019-02-28 21:47:37 -0500

    Removed keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MongoURI:
-    'mongodb://backendapi:Csgo699669!@ds111549.mlab.com:11549/socialmedia',
-  secretOrKey: 'secret',
-  cloudname: 'dozvpglka',
-  CloudinaryAPI: '733351435983291',
-  CloudinaryAPISecret: 'HLbjlIwFCLzBN0104bAQqK-2OCQ'
-};
